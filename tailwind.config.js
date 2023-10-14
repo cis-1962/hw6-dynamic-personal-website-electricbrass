c9496d49c90108ae4ab67729dfb08e9d6aa9ac22
Mia <miadawnmc@gmail.com>
2023-10-14 17:36:54 -0400

    Fix missing style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.tsx', 'index.html'],
+  purge: ['./src/**/*.tsx', 'index.html'],
   theme: {
     extend: {},
   },
