39c42d5726aa1d8646095bc30c7aa885ae5bc690
Mia <miadawnmc@gmail.com>
2023-10-14 17:41:31 -0400

    Fix missing style 2: electric boogaloo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.tsx', 'index.html'],
-  purge: ['./src/**/*.tsx', 'index.html'],
   theme: {
     extend: {},
   },
